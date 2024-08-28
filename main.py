c5b8d196ffc06af431c15a046f4391b6cf207cfc
RyanCheng98153 <ryan98153@gmail.com>
2024-08-28 16:40:09 +0800

    test: genetic algorithm solvers

--- main.py
+++ main.py
@@ -10,35 +10,35 @@ class Vehicle:
         self.BATTERY_CAPACITY: float = _battery
         
         self.soc: float = 100
+        self.remainEnergy: float = self.BATTERY_CAPACITY
         
-    def charge(self, _time:int, _energy:float ) -> float :
-        self.soc += _energy / self.BATTERY_CAPACITY
-        price = self.__getChargePrice(_time, _energy)
-        return price
-
-    def dischargeTravel(self, _distance: float):
+    def dischargeTravel(self, _distance: float) -> float:
         dischargeEnergy = self.__getDischargeEnergy( _distance)
         self.soc -= dischargeEnergy / self.BATTERY_CAPACITY
+        return dischargeEnergy / self.BATTERY_CAPACITY
     
     def __getDischargeEnergy(self, _distance: float):
         ALPHA = 0.6
         return ALPHA * self.WEIGHT * _distance
     
-    def __getChargePrice(_time: int, _energy: float ):
+    def getChargePrice(self, _time: int ):
         PRICE_PER_CHARGE: float = 1.0
         NIGHT_PRICE_RATIO: float = 2.0
         
-        price = _energy * PRICE_PER_CHARGE
+        price = self.remainEnergy * PRICE_PER_CHARGE
         
         if _time % 24 < 9: # NIGHT Price
             return price * NIGHT_PRICE_RATIO
         return price # DAY Price
     
     
+        
+            
+        
 class Schedule:
     def __init__(self, _fleets: list[Vehicle] = [] ):
         self.fleets: list[Vehicle] = _fleets
-        
+        self.HEALTH_SOC: list[float] = [20, 80]
         
     def setFleets(self, _fleet: list[Vehicle] ):
         for vehicle in _fleet:
@@ -56,8 +56,6 @@ def main():
         Vehicle( 3, 3000, 1000 ),
         Vehicle( 4, 3000, 1000 ),
     ])
-    
-    
     
 if __name__ == "__main__":
     main()
