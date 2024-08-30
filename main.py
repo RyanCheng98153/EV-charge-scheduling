7bc2b0843fc8cdb6becbc72515a3a5d6f2be27e5
RyanCheng98153 <ryan98153@gmail.com>
2024-08-30 09:27:39 +0800

    feat: naming each function in vehicle class

--- main.py
+++ main.py
@@ -8,37 +8,74 @@ class Vehicle:
         self.ID: int = id
         self.WEIGHT: float = _weight
         self.BATTERY_CAPACITY: float = _battery
+        self.HEALTH_SOC = [0.2, 0.8]
         
         self.soc: float = 100
-        self.remainEnergy: float = self.BATTERY_CAPACITY
-        
-    def dischargeTravel(self, _distance: float) -> float:
-        dischargeEnergy = self.__getDischargeEnergy( _distance)
-        self.soc -= dischargeEnergy / self.BATTERY_CAPACITY
-        return dischargeEnergy / self.BATTERY_CAPACITY
+        self.remain_energy: float = self.BATTERY_CAPACITY
     
-    def __getDischargeEnergy(self, _distance: float):
-        ALPHA = 0.6
-        return ALPHA * self.WEIGHT * _distance
+    def  getSOC(self):
+        return self.remain_energy / self.BATTERY_CAPACITY
     
-    def getChargePrice(self, _time: int ):
+    # E_t
+    def func1_getChargeCost(self, _energy, _time: int ):
         PRICE_PER_CHARGE: float = 1.0
         NIGHT_PRICE_RATIO: float = 2.0
         
-        price = self.remainEnergy * PRICE_PER_CHARGE
+        _energy = self.remainEnergy
+        price = _energy * PRICE_PER_CHARGE
         
         if _time % 24 < 9: # NIGHT Price
             return price * NIGHT_PRICE_RATIO
         return price # DAY Price
     
+    # W_t
+    def func2_getWearCost(self, _degradation: float, _value: float):
+        return _degradation * _value
     
+    # P_cs
+    def func3_getStationCost(self):
+        station_price = 1.0
+        return station_price
         
-            
+    # EC_v
+    def func7_getTravelEnergyCost(self, _distance: float) -> float:
+        ALPHA = 0.6
+        dischargeEnergy = ALPHA * self.WEIGHT * _distance
+        
+        return dischargeEnergy
+    
+    # e_vt
+    def func8_getReturnStationEnergy(self, _remain_energy, _travel_energy_cost, _distance):
+        remain_energy = self.remain_energy
+        travel_energy_cost = self.func7_getTravelEnergyCost(_distance)
+        return remain_energy - travel_energy_cost, travel_energy_cost
+    
+    # D(e_vt)
+    def func9_getDegradation(self, _remain_energy):
+        HEALTH_SOC_MIN, HEALTH_SOC_MAX  = self.HEALTH_SOC
+        DEGRADATION = 0.8
+        
+        if HEALTH_SOC_MIN <= self.soc <= HEALTH_SOC_MAX:
+            return DEGRADATION # health-charged
+        
+        return DEGRADATION * 2 # over-charged
+    
+    # DCH
+    def func11_getHealthDegradation(self, _remain_energy, _energy_cost):
+        cycle_life = 1
+        DoD = _energy_cost / self.BATTERY_CAPACITY
+        HEALTH_SOC_MIN, HEALTH_SOC_MAX  = self.HEALTH_SOC
+        
+        numerator = abs( HEALTH_SOC_MAX - self.remain_energy )
+        denominator = cycle_life * (DoD / 1.0) * self.BATTERY_CAPACITY
+        
+        health_degradation = numerator / denominator
+        return health_degradation
         
 class Schedule:
     def __init__(self, _fleets: list[Vehicle] = [] ):
         self.fleets: list[Vehicle] = _fleets
-        self.HEALTH_SOC: list[float] = [20, 80]
+        # self.HEALTH_SOC: list[float] = [20, 80]
         
     def setFleets(self, _fleet: list[Vehicle] ):
         for vehicle in _fleet:
