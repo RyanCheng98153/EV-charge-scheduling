26ddf9922e49444c07097b489555fd72bfa6e24e
RyanCheng98153 <ryan98153@gmail.com>
2024-12-06 01:20:25 +0800

    feat: run shell script

--- run.ps1
+++
@@ -1,6 +0,0 @@
-# Usage: .\run.ps1
-
-python .\main.py -m genetic -n 10 -o ./results/result_10.json
-python .\main.py -m genetic -n 100 -o ./results/result_100.json
-python .\main.py -m genetic -n 1000 -o ./results/result_1000.json
-python .\main.py -m genetic -n 10000 -o ./results/result_10000.json
---
+++ run.sh
@@ -0,0 +1,5 @@
+for n in 1 10 100 500 1000 2000 5000 10000
+do
+    python ./main.py --days 1 --mode genetic -n $n -o ./results/1day/result_day1_gene_$n.json
+    python ./main.py --days 7 --mode genetic -n $n -o ./results/7day/result_day7_gene_$n.json
+done
